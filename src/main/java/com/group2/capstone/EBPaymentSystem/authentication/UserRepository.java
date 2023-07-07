ed9a7581bfb21a1de6b2203418aa8d538de28146
Aniruth <22245222+Aniruth-raman@users.noreply.github.com>
2023-07-07 21:20:28 +0530

    Authentication JWT

--- src/main/java/com/group2/capstone/EBPaymentSystem/authentication/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group2.capstone.EBPaymentSystem.authentication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
